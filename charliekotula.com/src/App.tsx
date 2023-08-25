2fc093a65c2b9e6248a237e463de4c98bf445620
Charlie Kotula <charlie.kotula@gmail.com>
2023-08-25 11:15:23 -0500

    adding MUI in correct directory

--- charliekotula.com/src/App.tsx
+++ charliekotula.com/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from "@mui/material/Button";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button variant="contained">Contained</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
